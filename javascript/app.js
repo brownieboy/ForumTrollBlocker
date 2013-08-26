c9032ec7d985b628ccd2b3d27d4fc749667e5f62
Michael Brown <brownieboy@gmail.com>
2013-08-26 21:39:52 +1000

    First real working version, ZDNet only.

--- javascript/app.js
+++ javascript/app.js
@@ -14,33 +14,24 @@ myFilter.globals = myFilter.globals || {};
 // function check to see if both of those name chunks are in the Author name.
 // myFilter.globals.filterList = [["william farrel"], ["owl", "net"], ["toddbottom"], ["loverock", "davidson"]];
 
-myFilter.functions.isTroll = function(author) {
+myFilter.functions.isTroll = function(authorOriginal) {
 	// Loop throuh the master filter list, which is an array of arrays (see above).
+	var author = authorOriginal.toLowerCase();
 	var isMember;
-	console.log("isTroll: Retrieved filter list is " + JSON.stringify(myFilter.globals.filterList));
 	if (!myFilter.globals.filterList) {
 		console.log("isTroll: Retrieved filter list is undefined, exiting function.");
 		return false;
 	}
 	// Outer loop - the array of troll names to check against
-	myFilter.globals.filterList.some(function(memberValue) {
+	myFilter.globals.filterList.some(function(memberArray) {
 		isMember = true;
-		var memberArray = [];
-		console.log("memberValue = " + memberValue);
-		if (!$.isArray(memberValue)) {
-			memberArray.push(memberValue);
-		}
-		else {
-			memberArray = memberValue;
-		}
-		
-		console.log("memberArray = " + memberArray);
+	//	console.log("memberArray = " + memberArray);
 		memberArray.forEach(function(member) {
-			console.log("Processing member " + member);
+		//	console.log("Processing member " + member);
 			// Inner loop - the individual parts of the troll's name is an array too,
 			// although that array may have only one member.  We return true if
 			// *every part* of the troll's name is containe within the author's name.
-			if (author.indexOf(member) === -1) {
+			if (author.indexOf(member.toLowerCase()) === -1) {
 				// If only one part of the troll's name is not in the author's name
 				// then we don't have a match.
 				isMember = false;
@@ -56,10 +47,12 @@ myFilter.functions.isTroll = function(author) {
 myFilter.functions.processTrolls = function($authors) {
 	// Loop through all the authors and check them off against our master troll list
 	// via the isTroll() function.
-	console.log("processing trolls...");
+	// console.log("processing trolls...");
 	var author;
 	$authors.each(function(index, $author) {
 		author = $($author).html();
+	//	console.log("Processing author " + author);
+
 		if (myFilter.functions.isTroll(author.toLowerCase())) {
 			// If author is a troll, then we block the parent div that has the
 			// ".commentWrapper" class.  You can change this ".comment" and it
@@ -75,26 +68,42 @@ myFilter.functions.processTrolls = function($authors) {
 $(function() {
 	console.log("Forum troll blocker started");
 	chrome.storage.sync.get('forumBlockerSettings', function(settings) {
+		if(typeof settings.forumBlockerSettings.trollList !== "undefined") {
 		console.log("processing array");
-		var currentID, newValues;
-		var processedArray = [];
-		$.each(settings.forumBlockerSettings.trollList["zdnetTrollList"], function(index, value) {
-			if(value.indexOf(",") > -1) {
-				processedArray.push(value.split(","));
+			var currentID, newValues;
+			var processedArray = [];
+			var tempArray = [];
+			var hasTrolls = false;
+			// Get trolls from the Chrome storage.  Process the multi-part names, which are
+			// comma-delimited strings, into an array of arrays.
+			$.each(settings.forumBlockerSettings.trollList["zdnetTrollList"], function(index, value) {
+				if(value.indexOf(",") > -1) {
+					processedArray.push(value.split(","));
+					hasTrolls = true;
+				}
+				else {
+					if(value !== "") {
+						tempArray = [];
+						tempArray.push(value);
+						processedArray.push(tempArray);
+						hasTrolls = true;
+					}
+				}
+			});
+			myFilter.globals.filterList = processedArray;
+			console.log("Just retrieved filter list is " + JSON.stringify(myFilter.globals.filterList));
+			if(hasTrolls) {
+				myFilter.functions.processTrolls($("#comments .author"));
+				// waitForKeyElements runs on Ajax loaded data, so it gets triggered
+				// by the Previous and Next buttons, instead of just when the DOM is
+				// first loaded.
+				// We want to monitor all divs with the "author" class that are inside
+				// the id="comments" block.
+				waitForKeyElements("#comments .author", myFilter.functions.processTrolls);
 			}
 			else {
-				processedArray.push(value);
+				console.log("No troll list defined");
 			}
-		});
-				myFilter.globals.filterList = processedArray;
-		console.log("Just retrieved filter list is " + JSON.stringify(myFilter.globals.filterList));
-		myFilter.functions.processTrolls($("#comments .author"));
-				// waitForKeyElements runs on Ajax loaded data, so it gets triggered
-// by the Previous and Next buttons, instead of just when the DOM is
-// first loaded.
-// We want to monitor all divs with the "author" class that are inside
-// the id="comments" block.
-waitForKeyElements("#comments .author", myFilter.functions.processTrolls);
-
+		}
 	});
 });
